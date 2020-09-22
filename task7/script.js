d3ec9a9b2b38e03fa9170e0837df4b2f23b7fe18
YalavarthiSupriya <70761438+YalavarthiSupriya@users.noreply.github.com>
2020-09-22 16:43:30 +0530

    Create script.js

---
+++ task7/script.js
@@ -0,0 +1,15 @@
+//giving value to variable myage
+var myage=19;
+//assign earlyyears to 2
+var earlyyears=2;
+ earlyyears*=10.5;
+ //assign earlyyears to myage-2
+ var lateryears=myage-2;
+ //multiply lateryears by 4
+ lateryears=lateryears*4
+ console.log(earlyyears,lateryears)
+ //stores two variables in variable
+ var myAgeInDogYears=earlyyears,lateryears
+var myName="Supriya".toLowerCase();
+//Printing our name,dogyears etc in console
+console.log("My name is" +myName+". I am "+myage +"years old in human years which is "+myAgeInDogYears+" years old in dog years.")
